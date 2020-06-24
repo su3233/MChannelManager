20972af11aed1fe6eaa2fa1a1c258a85ee4d2d90
sutongsheng <mr_su3200@sina.com>
2020-06-24 16:57:02 +0800

    1、编辑状态和非编辑状态显示样式 2、我的和其他点击时动画的添加和删除 3、从assets文件读取Json数据

--- app/src/main/java/com/mooc/mchannelmanager/MainActivity.java
+++ app/src/main/java/com/mooc/mchannelmanager/MainActivity.java
@@ -3,10 +3,19 @@ package com.mooc.mchannelmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.TranslateAnimation;
+import android.widget.AdapterView;
 import android.widget.GridView;
 import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 
 import com.mooc.mchannelmanager.adapter.ChannelAdapter;
@@ -20,12 +29,19 @@ import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements AdapterView.OnItemClickListener {
     private static final String TAG = "MainActivity";
     private static final String JSON_FILE_NAME = "data.json";
+    private static final int ANIM_DURATION = 300;
     private List<String> userList = new ArrayList<>();
     private List<String> otherList = new ArrayList<>();
     private Context mContext;
+    private TextView tvMore;
+    private GridView gv_user;
+    private GridView gv_other;
+    private ChannelAdapter userAdapter;
+    private ChannelAdapter otherAdapter;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,9 +76,142 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView() {
         ImageView iv_edit = findViewById(R.id.iv_edit);
-        GridView gv_my = findViewById(R.id.gv_my);
-        GridView gv_other = findViewById(R.id.gv_more);
-        gv_my.setAdapter(new ChannelAdapter(mContext, userList));
-        gv_other.setAdapter(new ChannelAdapter(mContext, otherList));
+        tvMore = findViewById(R.id.tv_more);
+        gv_user = findViewById(R.id.gv_my);
+        gv_other = findViewById(R.id.gv_more);
+        userAdapter = new ChannelAdapter(mContext, userList, true);
+        otherAdapter = new ChannelAdapter(mContext, otherList, false);
+
+        gv_user.setAdapter(userAdapter);
+        gv_other.setAdapter(otherAdapter);
+        gv_user.setOnItemClickListener(this);
+        gv_other.setOnItemClickListener(this);
+
+        iv_edit.setOnClickListener(new View.OnClickListener() {
+            @Override
+                public void onClick(View view) {
+                toogleEditState();
+                ((ImageView) view).setImageResource(ChannelAdapter.isEditStatus ?
+                        R.drawable.ic_icon_edit_ok : R.drawable.ic_icon_edit);
+            }
+        });
     }
+
+    /**
+     * 切换编辑状态
+     */
+    private void toogleEditState() {
+        boolean isEdit = ChannelAdapter.isEidt();
+        ChannelAdapter.setEdit(!isEdit);
+
+        tvMore.setVisibility(isEdit ? View.INVISIBLE : View.VISIBLE);
+        gv_other.setVisibility(isEdit ? View.INVISIBLE : View.VISIBLE);
+        userAdapter.notifyDataSetChanged();
+        otherAdapter.notifyDataSetChanged();
+    }
+
+    /**
+     * 给View添加移动动画
+     *
+     * @param moveView
+     * @param startPos
+     * @param endPos
+     * @param duration
+     */
+    private void moveAnimation(final View moveView, int[] startPos, int[] endPos, int duration) {
+        TranslateAnimation translateAnimation = new TranslateAnimation(startPos[0], endPos[0], startPos[1], endPos[1]);
+        translateAnimation.setDuration(duration);
+        translateAnimation.setFillAfter(false);
+        translateAnimation.setAnimationListener(new Animation.AnimationListener() {
+            @Override
+            public void onAnimationStart(Animation animation) {
+
+            }
+
+            @Override
+            public void onAnimationEnd(Animation animation) {
+                //动画结束移除cloneView
+                ((ViewGroup) moveView.getParent()).removeView(moveView);
+                resetAdapter();
+            }
+
+            @Override
+            public void onAnimationRepeat(Animation animation) {
+
+            }
+        });
+        moveView.startAnimation(translateAnimation);
+    }
+
+    /**
+     * 操作后重置状态
+     */
+    private void resetAdapter() {
+        userAdapter.setTranslating(false);
+        otherAdapter.setTranslating(false);
+        userAdapter.remove();
+        otherAdapter.remove();
+    }
+
+    /**
+     * 克隆view实现动画
+     *
+     * @param view
+     * @return
+     */
+    private ImageView getCloneView(View view) {
+//        view.destroyDrawingCache();
+//        view.setDrawingCacheEnabled(true);
+//        Bitmap cache=Bitmap.createBitmap(view.getDrawingCache());
+//        ImageView imageView=new ImageView(this);
+//        imageView.setImageBitmap(cache);
+
+        Bitmap bitmap = Bitmap.createBitmap(view.getWidth(), view.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        ImageView imageView = new ImageView(this);
+        return imageView;
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+        //如果是编辑状态处理删除和增加操作
+        //不是编辑状态弹出Toast，事记使用中可以换成频道详情页跳转
+        if (ChannelAdapter.isEditStatus) {
+            GridView currentView;
+            final GridView anotherView;
+            if (adapterView == gv_user) {
+                currentView = gv_user;
+                anotherView = gv_other;
+            } else {
+                currentView = gv_other;
+                anotherView = gv_user;
+            }
+            //计算起点
+            final int[] startPos = new int[2];
+            final int[] endPos = new int[2];
+            view.getLocationInWindow(startPos);
+
+            ChannelAdapter currnetAdapter = (ChannelAdapter) currentView.getAdapter();
+            ChannelAdapter anotherAdapter = (ChannelAdapter) anotherView.getAdapter();
+            //添加在动画之前，删除在动画之后
+            anotherAdapter.setTranslating(true);
+            anotherAdapter.add(currnetAdapter.setRemove(position));
+            final ImageView cloneView = getCloneView(view);
+            ((ViewGroup) getWindow().getDecorView()).addView(cloneView,
+                    ViewGroup.LayoutParams.WRAP_CONTENT, ViewGroup.LayoutParams.WRAP_CONTENT);
+            currentView.post(new Runnable() {
+                @Override
+                public void run() {
+                    View lastView = anotherView.getChildAt(anotherView.getCount() - 1);
+                    lastView.getLocationInWindow(endPos);
+                    moveAnimation(cloneView, startPos, endPos, ANIM_DURATION);
+                }
+            });
+        } else {
+            Toast.makeText(mContext, userList.get(position), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
 }
--- app/src/main/java/com/mooc/mchannelmanager/adapter/ChannelAdapter.java
+++ app/src/main/java/com/mooc/mchannelmanager/adapter/ChannelAdapter.java
@@ -1,6 +1,7 @@
 package com.mooc.mchannelmanager.adapter;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -19,12 +20,37 @@ import java.util.List;
  * @Describe
  */
 public class ChannelAdapter extends BaseAdapter {
+    private static final String TAG = "ChannelAdapter";
     private Context context;
     private List<String> datas;
+    /**
+     * 是否可编辑
+     */
+    public static boolean isEditStatus;
+    public boolean isUser;
+    /**
+     * 标记删除位置
+     */
+    private int readyRemove = -1;
+    private AnimState animState = AnimState.EDIL;
 
-    public ChannelAdapter(Context context, List<String> datas) {
+    /**
+     * 定义动画状态，空闲，移动中
+     */
+    enum AnimState {
+        EDIL,
+        TRANSTING
+    }
+
+    /**
+     * @param context
+     * @param datas
+     * @param isUser  判断是那个grideView
+     */
+    public ChannelAdapter(Context context, List<String> datas, boolean isUser) {
         this.context = context;
         this.datas = datas;
+        this.isUser = isUser;
     }
 
     @Override
@@ -43,17 +69,97 @@ public class ChannelAdapter extends BaseAdapter {
     }
 
     @Override
-    public View getView(int position, View content, ViewGroup viewGroup) {
-        RelativeLayout layout = (RelativeLayout) LayoutInflater.from(context).inflate(R.layout.item_channel, null);
-        TextView tv_item = layout.findViewById(R.id.tv_item);
-        ImageView iv_item = layout.findViewById(R.id.iv_item_edit);
-        tv_item.setText(datas.get(position));
-
-        if (position % 2 == 0) {
-            iv_item.setImageResource(R.drawable.ic_icon_add);
-        } else {
-            iv_item.setImageResource(R.drawable.ic_icon_delete);
+    public View getView(int position, View contentView, ViewGroup viewGroup) {
+        if (contentView == null) {
+            contentView = LayoutInflater.from(context).inflate(R.layout.item_channel, null);
         }
-        return layout;
+        ViewHolder viewHolder = (ViewHolder) contentView.getTag();
+        if (viewHolder == null) {
+            viewHolder = new ViewHolder(contentView);
+            contentView.setTag(viewHolder);
+        }
+
+        viewHolder.tv_item.setText(datas.get(position));
+//根据当前是否为编辑状态，显示操作类型
+        if (isEditStatus) {
+            Log.e(TAG, "isUser: " + isUser);
+            viewHolder.iv_item.setVisibility(View.VISIBLE);
+            viewHolder.iv_item.setImageResource(isUser ? R.drawable.ic_icon_delete : R.drawable.ic_icon_add);
+        } else {
+            viewHolder.iv_item.setVisibility(View.INVISIBLE);
+        }
+        //判断currentView的状态
+//        处理anotherView状态
+        if (readyRemove == position || animState == AnimState.TRANSTING && position == getCount() - 1) {
+            viewHolder.tv_item.setText("");
+            viewHolder.tv_item.setSelected(true);
+            viewHolder.iv_item.setVisibility(View.INVISIBLE);
+        } else {
+            viewHolder.tv_item.setText(datas.get(position));
+            viewHolder.tv_item.setSelected(false);
+        }
+        return contentView;
     }
+
+    /**
+     * 添加数据
+     *
+     * @param name
+     */
+    public void add(String name) {
+        datas.add(name);
+        notifyDataSetChanged();
+    }
+
+    public void setTranslating(boolean translating) {
+        animState = translating ? AnimState.TRANSTING : AnimState.EDIL;
+    }
+
+    /**
+     * 添加删除标记
+     *
+     * @param index
+     */
+    public String setRemove(int index) {
+        readyRemove = index;
+        notifyDataSetChanged();
+        return datas.get(index);
+    }
+
+    public void remove() {
+        remove(readyRemove);
+        readyRemove = -1;
+    }
+
+    /**
+     * 删除某一个
+     *
+     * @param index
+     */
+    public void remove(int index) {
+        if (index > 0 && index < datas.size()) {
+            datas.remove(index);
+        }
+        notifyDataSetChanged();
+    }
+
+    public static void setEdit(boolean isEdit) {
+        isEditStatus = isEdit;
+    }
+
+    public static boolean isEidt() {
+        return isEditStatus;
+    }
+
+    private class ViewHolder {
+        TextView tv_item;
+        ImageView iv_item;
+
+        public ViewHolder(View view) {
+            tv_item = view.findViewById(R.id.tv_item);
+            iv_item = view.findViewById(R.id.iv_item_edit);
+        }
+    }
+
+
 }
